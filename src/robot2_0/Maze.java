84eb88aa51ec7256fbdc62598681bcad5b4fb67c
Erik Nordström <tfy15enm@cs.umu.se>
2021-04-25 16:17:12 +0200

    ROBOT 2.0: Add package

---
+++ src/robot2_0/Maze.java
@@ -0,0 +1,120 @@
+package robot2_0;
+
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * This class represents a Maze that reads a maze from a file into an array list. The maze
+ * keeps track of its size, start position and can check if a {@code Position} is movable or even a goal position.
+ *
+ * @author Erik Nordström (tfy15enm)
+ */
+public class Maze {
+
+    private final ArrayList<String> maze;
+
+    private int NR_OF_ROWS;
+    private int NR_OF_COLUMNS;
+    private Position start;
+
+    /**
+     * Generating the maze from file reader
+     *
+     * @param input the file reader to read from
+     */
+    public Maze(Reader input) {
+        boolean goalExists = false;
+        NR_OF_ROWS = 0;
+        NR_OF_COLUMNS = 0;
+
+        maze = new ArrayList<>();
+        Scanner scanner = new Scanner(input);
+
+        while (scanner.hasNext()) {
+            String currentLine = scanner.nextLine();
+
+            for (int i = 0; i < currentLine.length(); i++) {
+                char currentChar = currentLine.charAt(i);
+                if (start == null && currentChar == 'S') {
+                    start = new Position(i, NR_OF_ROWS);
+                } else if (start != null && currentChar == 'S') {
+                    throw new RuntimeException("MAZE ERROR: Multiple start positions is not allowed.");
+                }
+                if (currentChar == 'G') {
+                    goalExists = true;
+                }
+            }
+            if (NR_OF_COLUMNS < currentLine.length()) {
+                NR_OF_COLUMNS = currentLine.length();
+            }
+            maze.add(currentLine);
+            NR_OF_ROWS++;
+        }
+
+        if (!goalExists) {
+            throw new RuntimeException("MAZE ERROR: Goal of maze does not exist.");
+        }
+        if (start == null) {
+            throw new RuntimeException("MAZE ERROR: Start position of maze does not exist.");
+        }
+    }
+
+    /**
+     * Return true if the position is movable to from this {@code Position}.
+     * @param position the position we want to check if movable
+     * @return boolean
+     */
+    public boolean isMovable(Position position) {
+        if (position.getX() < 0 || position.getX() >= NR_OF_COLUMNS ||
+                position.getY() < 0 || position.getY() >= NR_OF_ROWS) {
+            return false;
+        }
+        char newChar = getChar(position);
+        return newChar == ' ' || newChar == 'G' || newChar == 'S';
+    }
+
+    /**
+     * Return true if the position is a goal of this {@code Maze}.
+     * @param position the position we want to check if goal
+     * @return boolean
+     */
+    public boolean isGoal(Position position) {
+        char newChar = getChar(position);
+        return newChar == 'G';
+    }
+
+    /**
+     * Returns the start position of this {@code Maze}
+     * @return Position
+     */
+    public Position getStart() {
+        return start;
+    }
+
+    /**
+     * Returns the maximum number of columns of this {@code Maze}
+     * @return int
+     */
+    public int getNumColumns() {
+        return NR_OF_COLUMNS;
+    }
+
+    /**
+     * Returns the maximum number of rows of this {@code Maze}
+     * @return int
+     */
+    public int getNumRows() {
+        return NR_OF_ROWS;
+    }
+
+    /**
+     * Helper method
+     * @param position the position where we want to check the current char
+     * @return char
+     */
+    private char getChar(Position position) {
+        String row = maze.get(position.getY());
+        return row.charAt(position.getX());
+    }
+}
---
+++ src/robot2_0/Position.java
@@ -0,0 +1,129 @@
+package robot2_0;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * This class represents a location (position) in {@code (x, y)} coordinate space, specified in integer precision.
+ * This class is used for navigation within the maze or storing information of positions.
+ *
+ * @author Erik Nordström (tfy15enm)
+ */
+public class Position {
+
+    private final int x;
+    private final int y;
+
+    /**
+     * Constructs and initializes a point at the specified
+     * {@code (x, y)} location in the coordinate space.
+     * @param x the x-coordinate
+     * @param y the y-coordinate
+     */
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Returns the x-coordinate of this {@code Position}
+     * in integer precision.
+     * @return the x-coordinate of this {@code Position}
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Returns the y-coordinate of this {@code Position}
+     * in integer precision.
+     * @return the y-coordinate of this {@code Position}
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Returns the position south of this {@code Position}
+     * @return Position
+     */
+    public Position getPosToSouth() {
+        return new Position(x, y + 1);
+    }
+
+    /**
+     * Returns the position north of this {@code Position}
+     * @return Position
+     */
+    public Position getPosToNorth() {
+        return new Position(x, y - 1);
+    }
+
+    /**
+     * Returns the position west of this {@code Position}
+     * @return Position
+     */
+    public Position getPosToWest() {
+        return new Position(x - 1, y);
+    }
+
+    /**
+     * Returns the position east of this {@code Position}
+     * @return Position
+     */
+    public Position getPosToEast() {
+        return new Position(x + 1, y);
+    }
+
+
+    /**
+     * Return true if the position is adjacent to this {@code Position}.
+     * @param newPosition the position we want to check if adjacent
+     * @return boolean
+     */
+    public boolean isAdjacent(Position newPosition) {
+        if (!this.equals(newPosition)) {
+            return getAllAdjacent().contains(newPosition);
+        }
+        return false;
+    }
+
+    /**
+     * Return a list of all positions
+     * @return List<Position>
+     */
+    private List<Position> getAllAdjacent() {
+        return Arrays.asList(
+                new Position(x, y + 1),
+                new Position(x, y - 1),
+                new Position(x - 1, y),
+                new Position(x + 1, y)
+        );
+    }
+
+    /**
+     * Return true if the position in the argument is the same as this {@code Position}
+     *
+     * @param p the position object.
+     * @return boolean.
+     */
+    @Override
+    public boolean equals(Object p) {
+        if (p instanceof Position) {
+            Position pos = (Position) p;
+            return pos.x == x && pos.y == y;
+        }
+        return false;
+    }
+
+    /**
+     * Return the hash code of the {@code (x, y)} coordinates
+     *
+     * @return int.
+     */
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return (31 * hash + getX()) + (31 * hash + getY());
+    }
+}
