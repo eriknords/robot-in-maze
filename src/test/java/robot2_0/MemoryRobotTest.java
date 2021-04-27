a91aa0c8c62f9e0e84def3c23e370e36cccb6c6d
Erik Nordström <tfy15enm@cs.umu.se>
2021-04-27 23:40:55 +0200

    ROBOT 2.0: Refactor MemoryRobotTest

--- src/test/java/robot2_0/MemoryRobotTest.java
+++ src/test/java/robot2_0/MemoryRobotTest.java
@@ -1,41 +1,39 @@
 package robot2_0;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.util.Scanner;
+import java.io.*;
+import java.util.Objects;
 
 public class MemoryRobotTest {
 
-    private static int MOVES = 100;
+    private static final int MAX_MOVES = 100;
+    private static MemoryRobot robot;
+
+    @BeforeEach
+    public void beforeClass() throws FileNotFoundException {
+        // Hopefully this will work since maze.txt is located in test resources
+        String file = Objects.requireNonNull(MemoryRobotTest.class.getClassLoader()
+                .getResource("maze.txt")).getFile();
+        Reader mazeReader = new FileReader(file);
+
+        Maze maze = new Maze(mazeReader);
+        robot = new MemoryRobot(maze);
+    }
 
     @Test
     public void testMemoryRobot() {
+        // GIVEN a maze and the random robot
+        int moves = 0;
 
-        Scanner scanner = new Scanner(getClass().getResourceAsStream("Words.txt"));
-        try (Reader input = new FileReader("/home/erik/projects/robot-in-maze/src/main/robot2_0/labyrint.txt")) {
-            // GIVEN a valid maze
-            Maze maze = new Maze(input);
-            MemoryRobot robot = new MemoryRobot(maze);
-
-            // WHEN moving the robot unit goal is reached (or very high number of max moves for complex mazes)
-            while (!robot.hasReachedGoal() && MOVES < 100) {
-                robot.move();
-                MOVES++;
-
-                System.out.printf("(%d,%d)\n", robot.getPosition().getX(), robot.getPosition().getY());
-            }
-            // THEN verify that the goal has been reached (can't use JUnit when compiling in terminal)
-            if (robot.hasReachedGoal()) {
-                System.out.printf("SUCCESS: MemoryRobot reached the goal after %d moves\n", MOVES);
-            } else {
-                System.out.printf("FAIL: MemoryRobot did not manage to reach the goal after %d moves\n", MOVES);
-            }
-        } catch (IOException exception) {
-            exception.printStackTrace();
-            System.exit(0);
+        // WHEN moving the robot unit goal is reached (or very high number of max moves for complex mazes)
+        while (!robot.hasReachedGoal() && moves < MAX_MOVES) {
+            robot.move();
+            moves++;
         }
+        // THEN verify that the goal has been reached
+        Assertions.assertTrue(robot.hasReachedGoal());
     }
 }
