cd9f122062286bb5aa20dd1a467fc241589077a1
Erik Nordström <tfy15enm@cs.umu.se>
2021-04-27 23:39:47 +0200

    ROBOT 2.0: Restructure module sources

--- src/test/java/robot2_0/MemoryRobotTest.java
+++ src/test/java/robot2_0/MemoryRobotTest.java
@@ -1,37 +1,37 @@
 package robot2_0;
 
+import org.junit.jupiter.api.Test;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Scanner;
 
 public class MemoryRobotTest {
 
-    /**
-     * Command line test program of the RandomRobot.
-     * The first argument should be the maze file name.
-     *
-     * @param args Arguments
-     */
-    public static void main(String[] args) {
-        int moves = 0;
+    private static int MOVES = 100;
 
+    @Test
+    public void testMemoryRobot() {
+
+        Scanner scanner = new Scanner(getClass().getResourceAsStream("Words.txt"));
         try (Reader input = new FileReader("/home/erik/projects/robot-in-maze/src/main/robot2_0/labyrint.txt")) {
             // GIVEN a valid maze
             Maze maze = new Maze(input);
             MemoryRobot robot = new MemoryRobot(maze);
 
             // WHEN moving the robot unit goal is reached (or very high number of max moves for complex mazes)
-            while (!robot.hasReachedGoal() && moves < 100) {
+            while (!robot.hasReachedGoal() && MOVES < 100) {
                 robot.move();
-                moves++;
+                MOVES++;
 
                 System.out.printf("(%d,%d)\n", robot.getPosition().getX(), robot.getPosition().getY());
             }
             // THEN verify that the goal has been reached (can't use JUnit when compiling in terminal)
             if (robot.hasReachedGoal()) {
-                System.out.printf("SUCCESS: MemoryRobot reached the goal after %d moves\n", moves);
+                System.out.printf("SUCCESS: MemoryRobot reached the goal after %d moves\n", MOVES);
             } else {
-                System.out.printf("FAIL: MemoryRobot did not manage to reach the goal after %d moves\n", moves);
+                System.out.printf("FAIL: MemoryRobot did not manage to reach the goal after %d moves\n", MOVES);
             }
         } catch (IOException exception) {
             exception.printStackTrace();
